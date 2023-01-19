6d5d9b06510492f5545d04e81949d96cd0eddad2
Anatolij Goga <anzefal@gmail.com>
2023-01-19 22:23:14 +0200

    Added index js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './css/styles.css';
 import { fetchPictures } from './js/fetchPictures';
+import { renderQuery } from './js/render-query';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.btn-load-more');
+
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreButton.addEventListener('click', onLoadMoreBtn);
+
+function onSearchForm(event) {
+  event.preventDefault();
+  query = event.currentTurget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadMoreButton.classList.add('is-hidden');
+}
