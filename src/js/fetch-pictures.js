38ea4868b09250edddab0e045bcd7f45a66e1baf
Anatolij Goga <anzefal@gmail.com>
2023-01-30 14:13:48 +0200

    Fixed async/await

--- src/js/fetch-pictures.js
+++ src/js/fetch-pictures.js
@@ -8,6 +8,7 @@ async function fetchPictures(query, page, perPage) {
   const response = await axios.get(
     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   );
+
   const data = await response.data;
 
   return data;
